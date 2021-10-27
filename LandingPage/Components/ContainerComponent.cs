afc6fe11fff909bb7d2909be5ca1477cb2ba2fce
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2021-10-27 15:35:29 +0200

    Create ContainerComponent

---
+++ LandingPage/Components/ContainerComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Umbraco.Cms.Core.Composing;
+
+namespace LandingPage.Components
+{
+    public class ContainerComponent : IComponent
+    {
+        private readonly ILogger<ContainerComponent> _logger;
+        public ContainerComponent(ILogger<ContainerComponent> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Initialize()
+        {
+            _logger.LogInformation("Initialize ContainerComponent");
+            
+            AddContainers();
+        }
+
+        public void AddContainers()
+        {
+
+        }
+
+        public void Terminate()
+        {
+            _logger.LogInformation("Terminate ContainerComponent");
+        }
+    }
+}
---
+++ LandingPage/Compose/LandingPageComposer.cs
@@ -0,0 +1,15 @@
+﻿using LandingPage.Extensions;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace LandingPage.Compose
+{
+    public class LandingPageComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            // Add Configuration
+            builder.AddLandingPageConfiguration();
+        }
+    }
+}
